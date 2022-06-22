c100f7ec9d5ad23e5fe9d89c6b02161f199aaaee
Jack Stanley <jack@stanley5.com>
2022-06-21 21:42:06 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GoogleMapReact from 'google-map-react';
+import { useEffect, useState } from 'react';
+
+type LatLng = { lat: number, lng: number };
 
 function App() {
+  const ll = (lat: number, lng: number) => ({ lat, lng });
+  const [points, setPoints] = useState<LatLng[]>([]);
+  useEffect(() => {
+    setPoints([
+      ll(37.782, -122.447),
+      ll(37.782, -122.445),
+      ll(37.782, -122.443),
+      ll(37.782, -122.441),
+      ll(37.782, -122.439),
+      ll(37.782, -122.437),
+      ll(37.782, -122.435),
+      ll(37.785, -122.447),
+      ll(37.785, -122.445),
+      ll(37.785, -122.443),
+      ll(37.785, -122.441),
+      ll(37.785, -122.439),
+      ll(37.785, -122.437),
+      ll(37.785, -122.435)
+    ])
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_KEY || '' }}
+        defaultCenter={ll(37.782, -122.435)}
+        defaultZoom={11}
+        heatmapLibrary={true}
+        heatmap={{positions: points, options: {radius: 20, opacity: .6}}}
+        ></GoogleMapReact>
     </div>
   );
 }
